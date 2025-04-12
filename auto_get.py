a876e333557f77dae3985f48d746930f1a743ed6
tiandic <145258834+tiandic@users.noreply.github.com>
2025-04-12 13:38:39 +0800

    Added the --auto option, modified the README.md file.

--- auto_get.py
+++ auto_get.py
@@ -44,20 +44,21 @@ def call_function_from_module(function_name, *args, **kwargs):
 def get_command_line_args():
     parser = argparse.ArgumentParser(description="命令行工具的说明")
     parser.add_argument("-l","--limit",type=int,default=0,help="限制输出,如 '-l 3'即仅输出算法出现次数大于等于3的相关输出")
-    parser.add_argument("-i","--index",type=int,required=True,help="指定校验码在bytes类型时的下标")
+    parser.add_argument("-i","--index",type=int,default=0,help="指定校验码在bytes类型时的下标")
     parser.add_argument("-s","--skip" ,action='store_true',help="指定该参数,则跳过计算范围只有一个字符的情况")
     parser.add_argument("-f","--file",type=str,required=True,help="指定输入的文件路径")
+    parser.add_argument("--auto",action='store_true',help="尝试自动寻找校验码算法(自动模式下,不指定-l时,仅会输出完全符合所有数据块的算法)")
     args = parser.parse_args()
     return args
 
-def main():
-    args=get_command_line_args()
-    func_list=list_functions_in_module(check_code_algorithm_func)
-
+def get_one_checkcode_algorithm(args):
+    
     file=open(args.file,encoding="utf-8")
+    func_list=list_functions_in_module(check_code_algorithm_func)
 
     # 获取校验码下标
     checkcode_NO=args.index
+
 
     doc={}
     # 字典格式:
@@ -90,7 +91,7 @@ def main():
                             doc[doc_key]=doc[doc_key]+1
                         else:
                             doc[doc_key]=1
-
+    file.close()
     sorted_data = dict(sorted(doc.items(), key=lambda item: (-item[1], len(str(item[0])))))
 
     # 获取限制输出的指定
@@ -105,7 +106,35 @@ def main():
             # 排除实际校验码出现在用于计算校验码的范围中
             if not (checkcode_NO >=key[1] and checkcode_NO <key[2]):
                 if not (sk_one and key[2]-key[1]==1):
-                    print(f"{key}: {value}")
+                    print(f"index:{args.index} {key}: {value}")
+    return doc
+
+def main():
+    args=get_command_line_args()
+
+    # 自动模式
+    if args.auto:
+            with open(args.file) as f:
+                total_lines=f.readlines()
+                total_line_num=len(total_lines)
+            # print(total_lines[0])
+            with open(args.file) as f:
+                frist_line=f.readline().strip()
+                # print(type(frist_line))
+                line_len=int(len(frist_line)/2)
+            if args.limit==0:
+                # 自动模式下,不指定-l时,仅会输出完全符合所有数据块的算法
+                args.limit=total_line_num
+            # 将每一个字节都当成校验码
+            for i in range(line_len):
+                args.index=i
+                get_one_checkcode_algorithm(args)
+    else:
+        # 非自动模式
+        get_one_checkcode_algorithm(args)
+
+
+
 
 if __name__ == '__main__':
     main()
